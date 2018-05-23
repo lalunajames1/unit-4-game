3d0e71e189dc7456f00058c40f3bdc0ed7988acc
Luis Luna <luisluna@GHC-SEO-0032.globalhc.io>
2018-05-22 21:51:03 -0500

    Almost finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,118 +1,95 @@
-$(document).ready(function() {
+
+$(document).ready(function() { 
 
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
-var randNum = 0;
-
 var red = 0;
 var blue = 0;
-var yellow = 0;
-var green = 0;
+var yell = 0;
+var gree = 0;
+
+initializeGame()
 
 function initializeGame() {
     wins = 0;
     losses = 0;
     totalScore = 0;
-    red = 0;
-    blue = 0;
-    yellow = 0;
-    green = 0;
+    red = randNum();
+    blue = randNum();
+    yell = randNum();
+    gree = randNum();
+    RandNumTarget();
+    $("#red-button").attr("value", red); 
+    $("#blue-button").attr("value", blue);
+    $("#yellow-button").attr("value", yell); 
+    $("#green-button").attr("value", gree);
+    
 }
 
-function RandNumTarget(){
-     let randNum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-     console.log(randNum);
-     $("#random-num").text(randNum)
+function RandNumTarget() {
+     initalRandNum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+     console.log(initalRandNum);
+     $("#random-num").text(initalRandNum)
 }
 
-RandNumTarget()
-
-
-function redNumBtn() {
-     let randRedNum = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-     red = randRedNum;
-     console.log(red);
+function randNum() {
+    let randRedNum = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    console.log(randRedNum);
+    return randRedNum;
+   
 }
+  
+$(".btn").on("click", function() {
 
-redNumBtn()
-
-$("#red-button").on("click", function() {
-     $("#red-button").attr("value", red); 
-
-});
-
-
-function blueNumBtn() {
-     let randBlueNum = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-     blue = randBlueNum;
-     console.log(blue);
-     }
-    
-blueNumBtn()
-    
-$("#blue-button").on("click", function() {
-     $("#blue-button").attr("value", blue); 
-    
-     });
-
-
-function yellNumBtn() {
-      let randYellNum = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-      yell = randYellNum;
-      console.log(yell);
-        }
-        
-yellNumBtn()
-        
-$("#yellow-button").on("click", function() {
-      $("#yellow-button").attr("value", yell); 
-        
-     });
-
-function greeNumBtn() {
-      let randGreeNum = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-      gree = randGreeNum;
-      console.log(gree);
-        }
-        
-greeNumBtn()
-        
-$("#green-button").on("click", function() {
-      $("#green-button").attr("value", gree);
-
-      $("#green-button").ready(function(){
-
-      totalScore = Number(totalScore) + Number($(this).val());
+    if (losses === 5){
+        endGame()
+        return
+       
+    } else if (wins === 3) {
+        endGame()
+        return
+    }
+      totalScore = Number(totalScore) + Number($(this).attr("value"));
       $("#total").text(totalScore)
       console.log(totalScore)
-      });
-     });
-
+   
+      if (totalScore === initalRandNum) {
+        randNum();
+        wins++;
+        $("#win").text(wins); 
+        totalScore = 0; 
+        $("#total").text(totalScore); 
+        RandNumTarget();
+        
+      
+    
+    } else if (totalScore > initalRandNum) {
+        randNum();
+        losses++;
+        $("#loss").text(losses);
+        totalScore = 0;
+        $("#total").text(totalScore);
+        RandNumTarget();
+        
+    
+    } 
+      
+});
+        
+     function endGame() {
+         if (losses === 5) {
+         $("#game-over").text("YOU LOST!!!!");
+         var button = $("#button").html("<button>");
+         $("#button").append(button);
+         } else if (wins === 3) {
+         $("#game-over").text("YOU WON!!!!");
+       }
+    }
 
 });
 
- 
 
 
-/* 
-
-if (totalScore === randNum) {
-    wins++;
-    RandNumTarget();
-    redNumBtn();
-    blueNumBtn();
-    yellNumBtn();
-    greeNumBtn();
-
-} else if (totalScore > randNum) {
-    loss++;
-    RandNumTarget();
-    redNumBtn();
-    blueNumBtn();
-    yellNumBtn();
-    greeNumBtn();
-}
 
 
-*/
